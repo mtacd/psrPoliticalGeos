ca0c653bf918b58e78ae3d46cca4aa595cbcc84e
Drew Stanley <andrew.stanley@mtacd.org>
2024-07-24 11:53:44 -0400

    created new module for determining assets near stations

---
+++ psrNearStationUpdate.py
@@ -0,0 +1,11 @@
+import arcpy 
+
+import psrParentLatLongUpdate
+
+def nearStation():
+
+    # To allow overwriting outputs change overwriteOutput option to True.
+    arcpy.env.overwriteOutput = True
+
+    arcpy.ImportToolbox(r"c:\program files\arcgis\pro\Resources\ArcToolbox\toolboxes\Data Management Tools.tbx")
+    arcpy.ImportToolbox(r"c:\program files\arcgis\pro\Resources\ArcToolbox\toolboxes\Conversion Tools.tbx")
