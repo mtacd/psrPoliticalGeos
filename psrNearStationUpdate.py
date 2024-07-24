3cf2d2d40de39684debc88e5a1f1be8fef92f5d4
Drew Stanley <andrew.stanley@mtacd.org>
2024-07-24 13:42:46 -0400

    null and near station
    Added in code that finds all NULL lat/long records that also are near a station

--- psrNearStationUpdate.py
+++ psrNearStationUpdate.py
@@ -9,3 +9,22 @@ def nearStation():
 
     arcpy.ImportToolbox(r"c:\program files\arcgis\pro\Resources\ArcToolbox\toolboxes\Data Management Tools.tbx")
     arcpy.ImportToolbox(r"c:\program files\arcgis\pro\Resources\ArcToolbox\toolboxes\Conversion Tools.tbx")
+
+    inputFile = r"C:\Users\1292346\gisProjects\PSR\psrFinal\psrFinal.gdb\psrParentLatLong_updated"
+
+    # select only the assets with no lat/long
+    noLatLongInter = r"C:\Users\1292346\gisProjects\PSR\psrFinal\psrFinal.gdb\nearStationNoLatLong_inter"
+    arcpy.analysis.Select(
+        in_features=inputFile, 
+        out_feature_class=noLatLongInter, 
+        where_clause="LATITUDE IS NULL And LONGITUDE IS NULL")
+    
+
+    arcpy.analysis.Select(
+    in_features=noLatLongInter,
+    out_feature_class= r"C:\Users\1292346\gisProjects\PSR\psrFinal\psrFinal.gdb\nearStationNoLatLong",
+    where_clause="KEY_ASSET_NEAR_STATION IS NOT NULL"
+)
+
+nearStation()
+    
